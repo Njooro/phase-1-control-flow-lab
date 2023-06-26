3c4932560eedf111c86c264a9bafc6aa2b140767
Maxwell <njoromax5@gmail.com>
2023-06-26 13:05:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= '400'){
+    return 'This one is on me!'}
+    else if (ride >= '2000' && ride <= '2500'){
+      return 'I will gladly take your thirty bucks.'
+    } else{
+      return 'No can do.'
+    }  
+}
+function ternaryCheckCity(city){
+  let NYC
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+   switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'thanks for everything':
+      return 'Bye.'
+      break;
+    default: 'Bye.'
+      break;
+  }
 }
